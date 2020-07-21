aa6e1e1b6016bcc3eb58a88f1fe20b174fd1317d
Ajoy Deb Nath <debnath.ajoy1010@gmail.com>
2020-07-22 03:55:08 +0600

    ConfirmationActivity and navigate to createOrder

--- app/src/main/java/com/target/onlinecourierservice/ui/home/HomeFragment.java
+++ app/src/main/java/com/target/onlinecourierservice/ui/home/HomeFragment.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.target.onlinecourierservice.R;
+import com.target.onlinecourierservice.ui.CreateOrder.CreateOrderFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    Button parcel_order;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +36,22 @@ public class HomeFragment extends Fragment {
                 //textView.setText(s);
             }
         });
+
+        parcel_order=root.findViewById(R.id.parcel);
+        final NavController navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+
+
+        parcel_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_fragment,new CreateOrderFragment())
+                        .commit();*/
+                navController.navigate(R.id.nav_create_order);
+            }
+        });
+
         return root;
     }
 }
